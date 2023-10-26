d4f64cbead46dd49737d9edcce74925edb82d70f
Nikhil Verma <nikhilweee@gmail.com>
2023-10-26 00:35:19 -0400

    Update results in source code

--- src/execute.ts
+++ src/execute.ts
@@ -57,7 +57,11 @@ async function checkExecution(
             `${command} Results`,
             { preserveFocus: true, viewColumn: vscode.ViewColumn.Beside }
           );
-          panel.webview.html = parseExecutionResults(res, command, testJSON);
+          const results = parseResults(res, command, testJSON);
+          panel.webview.html = results.formatted;
+          updateResults(results.parsed)
+            .then()
+            .catch((error) => {});
           return;
         }
       }
@@ -150,11 +154,8 @@ export function parseEditor(requireTests = true) {
   return parsed;
 }
 
-function parseExecutionResults(
-  results: Object,
-  command: string,
-  testJSON: Object[]
-): string {
+function parseResults(results: Object, command: string, testJSON: Object[]) {
+  const html: Object = {};
   const parsed: Object = {};
 
   // Format Test Cases
@@ -163,21 +164,21 @@ function parseExecutionResults(
   const truths = pop(results, "expected_code_answer");
   const comparison = pop(results, "compare_result");
 
-  parsed.tests = "";
+  html.tests = "";
 
   if (
     typeof answers === "object" &&
     typeof truths === "object" &&
     typeof comparison === "string"
   ) {
-    parsed.tests = `
+    html.tests = `
     <h3>Test Cases</h3>
     `;
     answers.map((answer, i) => {
       const expected = truths[i];
       const compare = comparison[i] === "1" ? "🟢" : "🔴";
       const test = JSON.stringify(testJSON[i]);
-      parsed.tests += `
+      html.tests += `
       <pre>
       Input:   : ${test}
       Expected : ${expected}
@@ -201,17 +202,32 @@ function parseExecutionResults(
     } else if (typeof num_correct === "number" && num_correct > 0) {
       emoji = "🟡";
     }
-    parsed.heading = `
-    <h2>${command} ${status_msg}: ${num_correct} / ${num_total} ${emoji}</h2>`;
+    html.heading = `<h2>${command} ${status_msg}: `;
+    html.heading += `${num_correct} / ${num_total} ${emoji}</h2>`;
+    parsed.type = command;
+    parsed.status = status_msg.toString();
+    parsed.num_total = num_total.toString();
+    parsed.num_correct = num_correct.toString();
   } else {
-    parsed.heading = `<h2>${status_msg}</h2>`;
+    html.heading = `<h2>${command} ${status_msg}</h2>`;
+    parsed.type = command;
+    parsed.status = status_msg.toString();
   }
 
-  // Format Status
+  // Format Statuses
+
+  let runtime_status = pop(results, "status_runtime");
+  parsed.runtime_status = runtime_status.toString();
+  runtime_status = `<li>Runtime Status: ${runtime_status}</li>`;
+
+  let memory_status = pop(results, "status_memory");
+  parsed.memory_status = memory_status.toString();
+  memory_status = `<li>Memory Status: ${memory_status}</li>`;
 
   let runtime_percentile = pop(results, "runtime_percentile");
   if (typeof runtime_percentile === "number") {
     runtime_percentile = runtime_percentile.toFixed(5);
+    parsed.runtime_percentile = runtime_percentile.toString();
     runtime_percentile = `<li>Runtime Percentile: ${runtime_percentile}</li>`;
   } else {
     runtime_percentile = "";
@@ -220,16 +236,19 @@ function parseExecutionResults(
   let memory_percentile = pop(results, "memory_percentile");
   if (typeof memory_percentile === "number") {
     memory_percentile = memory_percentile.toFixed(5);
+    parsed.memory_percentile = memory_percentile.toString();
     memory_percentile = `<li>Memory Percentile: ${memory_percentile}</li>`;
   } else {
     memory_percentile = "";
   }
 
-  parsed.status = `
+  parsed.result_time = new Date().toJSON();
+
+  html.status = `
   <h3>${command} Status</h3>
   <ul>
-  <li>Runtime Status: ${pop(results, "status_runtime")}</li>
-  <li>Memory Status: ${pop(results, "status_memory")}</li>
+  ${runtime_status}
+  ${memory_status}
   ${runtime_percentile}
   ${memory_percentile}
   </ul>
@@ -237,11 +256,11 @@ function parseExecutionResults(
 
   // Format Errors
 
-  parsed.errors = "";
+  html.errors = "";
 
   const runtime_error = pop(results, "runtime_error");
   if (runtime_error) {
-    parsed.errors += `
+    html.errors += `
     <h3>Error</h3>
     <pre>${runtime_error}</pre>
     `;
@@ -249,7 +268,7 @@ function parseExecutionResults(
 
   const full_runtime_error = pop(results, "full_runtime_error");
   if (runtime_error) {
-    parsed.errors += `
+    html.errors += `
     <h3>Full Error</h3>
     <pre>${full_runtime_error}</pre>
     `;
@@ -257,7 +276,7 @@ function parseExecutionResults(
 
   // Format Details
 
-  parsed.details = `
+  html.details = `
   <details>
   <summary><strong>Other Information</strong></summary>
   <pre>${JSON.stringify(results, null, 2)}</pre>
@@ -275,16 +294,16 @@ function parseExecutionResults(
   <style>pre { white-space: pre-wrap; }</style>
   </head>
   <body>
-  ${parsed.heading}
-  ${parsed.status}
-  ${parsed.tests}
-  ${parsed.errors}
-  ${parsed.details}
+  ${html.heading}
+  ${html.status}
+  ${html.tests}
+  ${html.errors}
+  ${html.details}
   </body>
   </html>
   `;
 
-  return formatted;
+  return { parsed: parsed, formatted: formatted };
 }
 
 function pop(object: Object, key: string): string | number | [] {
@@ -294,4 +313,30 @@ function pop(object: Object, key: string): string | number | [] {
     delete object[key];
   }
   return value;
+}
+
+async function updateResults(parsed: Object) {
+  if (!vscode.window.activeTextEditor) {
+    throw new Error("No active editor found.");
+  }
+
+  const document = vscode.window.activeTextEditor.document;
+  const code = document.getText();
+
+  // For now, just restore previous solution
+  await vscode.window.activeTextEditor.edit((editBuilder) => {
+    const regex = RegExp(
+      "# vsleet:results:start(.*)# vsleet:results:end",
+      "gds"
+    );
+    const match = regex.exec(code);
+    if (!match || match.length < 2 || !match.indices) {
+      throw new Error("Cannot find result markers.");
+    }
+    const start = document.positionAt(match.indices[1][0]);
+    const end = document.positionAt(match.indices[1][1]);
+    const range = new vscode.Range(start, end);
+    const results = `\n${JSON.stringify(parsed, null, 2)}\n`;
+    editBuilder.replace(range, results);
+  });
 }
